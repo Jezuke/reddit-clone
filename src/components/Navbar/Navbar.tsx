af4a107b8eb6e90d1ae04d65c591d9533fb43ea0
Jezuke <jezaki48@gmail.com>
2023-01-30 21:34:17 -0600

    logo and Flex components

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Flex, Image } from '@chakra-ui/react';
+import React from 'react';
+
+const Navbar:React.FC = () => {
+    return <Flex bg="white" height="44px" padding="6px 12px">
+        <Flex align="center">
+            <Image src="/images/redditFace.svg" alt="reddit face" height="30px"/>
+            <Image src="/images/redditText.svg" 
+                   alt="reddit text" 
+                   height="46px" 
+                   display={{ base: "none", md: "unset"}}
+            />
+        </Flex>
+        {/* <Directory/>
+        <SearchInput/>
+        <RightContent/> */}
+    </Flex>
+}
+export default Navbar;
